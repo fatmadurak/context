cf30bbcbefa90a84057630155366856d4c2155a2
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-27 11:41:03 +0300

    c

--- src/component/ChangeTheme.js
+++ src/component/ChangeTheme.js
@@ -4,13 +4,14 @@ import { UseTheme } from '../context/ThemeContext'
 
 function ChangeTheme() {
  const {theme,setTheme}=UseTheme();
-
+ console.log(theme)
 
   return (
     <div>
 
 
     active theme:{theme}
+    
     <br/>
     <br/>
     <button onClick={()=>setTheme("dark")}>ChangeTheme</button>
